299559c2afcc0be6aeff040a031745f68d038516
Maxvincee <maxvince87@gmail.com>
2025-02-04 11:53:39 +0100

    Add : Title functions in MainActivity.kt

--- TP2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ TP2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +28,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold { innerPadding ->
+                    Title(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+
             }
         }
     }
@@ -38,10 +47,30 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun Title(modifier: Modifier = Modifier){
+    Box(
+        modifier = Modifier
+            .fillMaxSize() // Remplir tout l'espace disponible
+    ) {
+        // Contenu centré et déplacé vers le bas
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center) // Centrer le contenu horizontalement et verticalement
+                .height(500.dp)
+        ) {
+            Text(
+                text = "Bienvenue",
+                style = TextStyle(fontSize = 35.sp, fontWeight = FontWeight.Bold)
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Greeting("World")
     }
 }
